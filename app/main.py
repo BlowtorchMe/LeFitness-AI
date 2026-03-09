682b52730fd83ede33fa238795acbbcd47522240
Ellinor <ellinor.fexdal@icloud.com>
2026-03-09 11:12:04 +0100

    fix cors for frontend

--- app/main.py
+++ app/main.py
@@ -98,7 +98,10 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://le-fitness-ai-frontend.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
